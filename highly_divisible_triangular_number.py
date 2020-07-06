5b60cdc3641161f202a0adc63b57144798319e55
David Kistauri <davidkis113@gmail.com>
2020-07-06 11:47:03 +0300

    Problem 12

--- highly_divisible_triangular_number.py
+++ highly_divisible_triangular_number.py
@@ -1,61 +1,31 @@
-# First, first number of divisors
-# over 5000 is 5001 — odd number.
-# Only squares have odd numbers of divisors,
-# because we can divide number on
-# it's divisor and get new divisor —
-# pairs, but with square, if we
-# divide it by it's root, we get
-# it's root again — so it's not pair.
+# Project Euler
+# Problem 12
+# Highly divisible triangular number
 
-# Also we have a sequence of oddness
-# and eveness: two odd and two even
-# numbers.
 
-# This number must be even.
+import math
 
-# Distance between two neighbour
-# squares is a summ of their roots.
 
-# Or not. We can jump over even
-# number of divisors.
+def count_divs(x):
+    n = 0
+    for i in range(1, int(math.sqrt(x)) + 1):
+        if i**2 == x:
+            n += 1
+        elif x % i == 0:
+            n += 2
+    return n
 
-# 1
-# 1 3
-# 1 2 3 6
-# 1 2 5 10
-# 1 3 5 15
-# 1 3 7 21
-# 1 2 4 7 14 28
-# 1 2 3 4 6 9 12 18 36
-# 1 3 5 9 15 45
-# 1 5 11 55
-# 1 2 3 6 11 22 33 66
-# 1 2 3 6 13 26 39 78
 
-# 36 — 9
-# 1 2 3 4 6 9 12 18 36
+def highly_divisible_triangular_number(number_of_divisors):
+    n = 1
+    c = 1
+    i = 1
+    while True:
+        c += 1
+        n += c
+        if (count_divs(n) > number_of_divisors):
+            break
+    return n
 
-# 49 — 3
-# 1 7 49
 
-# 64 — 7
-# 1 2 4 8 16 32 64
-
-# 81 - 5
-# 1 3 9 27 1
-
-# 100
-# 1 2 4 5 10 20 25 50 100
-
-# 121
-# 1 11 121
-
-# 144
-# 1 2 3 4 6 8 9 12 16 18 24 36 48 72 144
-
-# 144 - 100 = (12 - 10)(12 + 10) = 2 * 22
-
-# 1
-# 4
-# 9
-# 16
+print(highly_divisible_triangular_number(500))
